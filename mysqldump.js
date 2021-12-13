4a88af462e4a27e46e4e6658ed33a2dabf918d03
mycve <mycve@qq.com>
2021-12-13 23:05:34 +0800

    first

---
+++ mysqldump.js
@@ -0,0 +1,26 @@
+const mysqldump = require('mysqldump')
+const argv = require('minimist')(process.argv.slice(2));
+
+console.info("[*] By: github")
+
+if(argv.h && argv.u&& argv.p&& argv.d&& argv.o){
+    const options = {
+        connection: {
+            host: argv.h,
+            user: argv.u,
+            password: argv.p,
+            database: argv.d
+        },
+        dumpToFile: argv.o
+    }
+    if(argv.t){
+        options['dump'] = {
+            "tables": argv.t.split(",")
+        }
+    }
+	mysqldump(options).then();
+}else{
+    console.info("[*] ./mysqldump -h 127.0.0.1 -u root -p 123456 -d database -o data.sql");
+    console.info("[*] ./mysqldump -h 127.0.0.1 -u root -p 123456 -d database -o data.sql -t table1,table2");
+}
+
